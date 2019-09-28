02260250978099d141ec0698db34df94db262670
Gordan Savicic <gordo@yugo.at>
2019-09-28 16:12:52 +0200

    initialized cypress

---
+++ cypress/integration/workshop_chronus/001_googlequery.js
@@ -0,0 +1,21 @@
+describe('test_name', function() {
+
+ it('performing a google search query', function() {
+
+    cy.viewport(1280, 1289)
+ 
+    cy.visit('https://www.google.com/')
+ 
+    // getting focus on input field for search query
+    cy.get('.A8SBwf > .RNNXgb > .SDkEP > .a4bIc > .gLFyf').click()
+ 
+    // typing into input field
+    cy.get('.A8SBwf > .RNNXgb > .SDkEP > .a4bIc > .gLFyf').type('hello world')
+ 
+    cy.get('.UUbT9 > .aajZCb > .VlcLAe > center > .gNO89b').click()
+ 
+    cy.visit('https://www.google.com/search?safe=off&source=hp&ei=y2iPXaXEDemSmwWMi6KQBQ&q=hello+world&oq=hello+world&gs_l=psy-ab.12..0l10.2520.4189..8045...0.0..0.104.1147.10j2....3..0....1..gws-wiz.....0..0i131.ilyYmeP-1Vc&ved=0ahUKEwjl1ZuG2PPkAhVpyaYKHYyFCFIQ4dUDCAY')
+ 
+ })
+
+})
